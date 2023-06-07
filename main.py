9953c5182344f6431e03da59f439cfa10607b494
Daud Dhiya <daud.dhiya31@gmail.com>
2023-06-07 21:41:35 +0700

    fix: string splitting

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ list_name = []
 
 f = open("recipients_number.txt", "r")
 for num in f:
-  number = '+' + num[:-1]
-  list_number.append(number)
+  if "\n" in num:
+    number = '+' + num[:-1]
+    list_number.append(number)
+  else: list_number.append('+' + num)
 
 f = open("recipients_name.txt", "r")
 for recipients in f:
-  list_name.append(recipients[:-1])
+  if "\n" in recipients:
+    list_name.append(recipients[:-1])
+  else: list_name.append(recipients)
 
 img = '/Users/macbook/Downloads/pywhatkit.jpg' # path through your image
 
